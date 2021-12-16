88e7ed0e60cce484a91f51cfef34e635be4552bf
Davit9494 <davitsahakyan94@mail.ru>
2021-12-17 00:21:46 +0400

    feat: Created index.html and t2_objDestructuring.js files

---
+++ t2_objDestructuring.js
@@ -0,0 +1,7 @@
+"use strict";
+let person = { name: "Toni Kroos", country: "Germany", job: "Footballer" };
+let { name, country, job } = person;
+
+console.log(name);
+console.log(country);
+console.log(job);
